12c86b407c0d34c4db5094066ce5a225fa169b5f
ANUSHREEDESAI1995 <dr.anushreedesai@gmail.com>
2020-06-10 09:34:42 +1000

    Modified an .rmd file to .R file

---
+++ PART1, 1.R
@@ -0,0 +1,20 @@
+# Downloaded the data of gene expression file - ANSWER 1
+download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/gene_expression.tsv",
+              destfile = "assessment3.tsv")
+# Reading the table and making sure that gene accession names are the row names
+originaldata <- read.table("assessment3.tsv")
+originaldata <- read.table("assessment3.tsv", header= TRUE, stringsAsFactors= FALSE, row.names=1) 
+# Data imported properly and printing first six rows and structure as well.
+head(originaldata)
+str(originaldata)
+# Command rowmeans is used to find the means of other columns - ANSWER 2
+rowMeans(originaldata)
+# A new column is made with the name of Meansofothercolumn to store these means
+Meansofothercolumn <- rowMeans(originaldata)
+# This column is bind to the main data now
+cbind(originaldata, Meansofothercolumn)
+# Permanent change to data. Adding the new column of means
+originaldata <- cbind(originaldata, Meansofothercolumn)
+# Observe the first six genes to check if new column is added
+head(originaldata)
+
