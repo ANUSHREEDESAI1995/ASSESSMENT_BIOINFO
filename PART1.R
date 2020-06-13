64a1be8ed2608af82d3c66e0081f844b3bbb37e9
ANUSHREEDESAI1995 <dr.anushreedesai@gmail.com>
2020-06-13 17:27:18 +1000

    In order to avoid unnecessary codes running, some were deleted to make precise .rmd file

--- PART1.R
+++ PART1.R
@@ -14,23 +14,18 @@ head(originaldata)
 str(originaldata)
 
 #ANSWER 2
-# Command rowmeans is used to find the means of other columns 
-rowMeans(originaldata)
-# A new column is made with the name of Meansofothercolumn to store these means
+
+# A new column is made with the name of Meansofothercolumn to store these means.
 Meansofothercolumn <- rowMeans(originaldata)
-# This column is bind to the main data now
-cbind(originaldata, Meansofothercolumn)
-# Permanent change to data. Adding the new column of means
+# Adding the new column of means.
+# Permanent change to data. 
 originaldata <- cbind(originaldata, Meansofothercolumn)
 # Observe the first six genes to check if new column is added
 head(originaldata)
 
 #ANSWER 3
-# The order commands is used in order to adjust any of the columns in ascending or descending order
-order( originaldata $ Meansofothercolumn)
-# The order of column Meansofothercolumn is arranged in descending order with the highest values first
-originaldata[order( - originaldata$Meansofothercolumn ) , ]
-# Permanent change of the descending order to Meansofothercolumn column has been made after this step
+
+# Permanent change of the descending order to Meansofothercolumn column has been made after arranging in descending step using the command order
 originaldata <- originaldata[order( - originaldata$Meansofothercolumn ) , ]
 # Extracting just the first ten rows with highest means
 row.names(originaldata[1:10,])
@@ -43,8 +38,9 @@ nrow(subset(originaldata, Meansofothercolumn < 10))
 #ANSWER 5
 # In order to plot a histogram command hist is used. 
 # i tried plotting it just normally first
-hist(originaldata$Meansofothercolumn)
-# But because the genes with mean say less than 100 are maxiumum the entire hishtogram was just plotted round that values.
+hist(originaldata$Meansofothercolumn, xlab="Mean of the genes", 
+     main = "MEAN V/S FREQUENCY")
+# But because the genes with mean say less than 25,000 are maxiumum the entire hishtogram was just plotted round that values.
 # Hence by limiting the genes with means in x axis from 20,000 to 25,000 
 # and frequency till 60 we could get a good histogram
 hist(originaldata$Meansofothercolumn, xlim= c(20000,250000), ylim= c(0,60), xlab="Mean of the genes", 
@@ -56,24 +52,15 @@ hist(originaldata$Meansofothercolumn, xlim= c(20000,250000), ylim= c(0,60), xlab
 download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/growth_data.csv", 
               destfile="assessment3part2.csv")
 originaldatapart2 <- read.csv("assessment3part2.csv")
-# Observing the structure of the data and using command head to print the data of first 6 rows
-head(originaldatapart2)
-str(originaldatapart2)
 # Command colnames will give us the result to check the column names.
 colnames(originaldatapart2)
 
 #ANSWER 7 
 # by the command subset i would separate the data for both the sites: northeast and southwest
-subset(originaldatapart2, Site== 'northeast')
 # Making permanent changes to the sites
 NORTHEAST <- subset(originaldatapart2, Site== 'northeast')
-subset(originaldatapart2, Site== 'southwest')
 SOUTHWEST <- subset(originaldatapart2, Site== 'southwest')
 # Calculating the mean of both the sites using command mean, for years 2004 and 2019
-mean(NORTHEAST$Circumf_2004_cm)
-mean(NORTHEAST$Circumf_2019_cm)
-mean(SOUTHWEST$Circumf_2004_cm)
-mean(SOUTHWEST$Circumf_2019_cm)
 # Calculating the standard deviation for both the sites using command sd, for years 2004 and years 2019
 sd(NORTHEAST$Circumf_2004_cm)
 sd(NORTHEAST$Circumf_2019_cm)
@@ -96,9 +83,6 @@ mean(SOUTHWEST$Circumf_2019_cm)
 
 # Obtaining the difference by subtracting circumference of year 2009 from year 2019 at site Northeast
 DiffNortheast <- NORTHEAST$Circumf_2019_cm - NORTHEAST$Circumf_2009_cm
-DiffNortheast
-# Adding new column to main NORTHEAST data
-cbind(NORTHEAST,DiffNortheast)
 # Making permanent change by adding a new column DiffNorthEast that is the difference in circumference of year 2009 and 2019
 NORTHEAST <-cbind(NORTHEAST,DiffNortheast)
 # Checking first six rows
@@ -106,9 +90,6 @@ head(NORTHEAST)
 
 # Obtaining the difference by subtracting circumference of year 2009 from year 2019 at site SouthWest
 DiffSouthwest <- SOUTHWEST$Circumf_2019_cm - SOUTHWEST$Circumf_2009_cm
-DiffSouthwest
-# Adding new column to main SouthWest data
-cbind(SOUTHWEST,DiffSouthwest)
 # Making permanent change by adding a new column DiffSouthWest that is the difference in circumference of year 2009 and 2019
 SOUTHWEST <- cbind(SOUTHWEST,DiffSouthwest)
 # Checking first six rows
